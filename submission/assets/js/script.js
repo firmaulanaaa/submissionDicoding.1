5902672506a2efe17b18318816421822ad6b0751
firmaulanaaa <59666390+firmaulanaaa@users.noreply.github.com>
2020-09-28 21:44:43 +0700

    Add files via upload
    ver.0.1

---
+++ submission/assets/js/script.js
@@ -0,0 +1,10 @@
+const slide = () => {
+	const burger = document.querySelector('.burger');
+	const nav =document.querySelector('.nav-links');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('active');
+	});
+}
+
+slide();
